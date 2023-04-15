892a53b780d7ca12a53b54e175d8830e33051420
huutribui <huu-tri.bui@intel.com>
2023-04-14 23:14:16 -0700

    Finished hw2

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,19 @@
 // Add your code here
+const originalText = document.getElementById('text').innerHTML;
 
-input.addEventListener('keydown', handleKeyDown);
+const handleKeyDown = (event) => {
+	document.getElementById('text').innerHTML = originalText;
+	const searchKey = document.getElementById('searchKey').value;
+	console.log('EVENT: ', searchKey);
+	const words = originalText.split(' ');
+	for (let i = 0; i < words.length; i++) {
+		if (words[i] === event.target.value)
+			words[i] = `<span style="background-color: yellow">${searchKey}</span>`;
+	}
+	console.log(words);
+	document.getElementById('text').innerHTML = words.join(' ');
+};
+
+const input = document.querySelector('input');
+
+input.addEventListener('keyup', handleKeyDown);
--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,26 @@
 // Add your code here
+const inputForm = document.getElementById('inputForm');
+inputForm.addEventListener('submit', function (event) {
+	event.preventDefault();
+	let name = document.getElementById('fullname').value;
+	let email = document.getElementById('email').value;
+	let comment = document.getElementById('comment').value;
+	let os = document.getElementById('os').checked;
+	let pl = document.getElementById('pl').checked;
+	let fs = document.getElementById('fs').checked;
+	console.log(
+		'============================Submit Details============================\n'
+	);
+	console.log('Name:', name);
+	console.log('Email:', email);
+	if (comment) console.log('Comment:', comment);
+	if (os || pl || fs) {
+		console.log('Course(s) taken: ');
+		if (os) console.log('\t Operating System');
+		if (pl) console.log('\t Programming Languages');
+		if (fs) console.log('\t Full Stack Web Development');
+	}
+	console.log(
+		'\n=============================================================='
+	);
+});
